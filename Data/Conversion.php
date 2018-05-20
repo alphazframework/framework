3d8bcf608802056ee946dc64dbd8ca1d80763897
malikumer <lablnet01@gmail.com>
2018-05-20 12:46:18 +0500

    adding data conversion class

---
+++ Data/Conversion.php
@@ -0,0 +1,66 @@
+<?php 
+namespace Softhub99\Zest_Framework\Data;
+
+class Conversion
+{
+
+		/**
+		 * Convert arrays to Object
+		 *
+		 * @param array $array Arrays
+		 *
+		 * @return object
+		 */
+	public static function ArrayObject($array){
+		if(is_array($array)){
+			$object = new stdClass();
+			foreach ($array as $key => $value){
+				if(is_array($value)){
+					$object->$key = DataType::ArrayObject($value);
+				}else{
+			    	$object->$key = $value;
+				}
+			}
+			return (object) $object;
+		}else{
+			return false;
+		}
+	}
+		/**
+		 * Convert Objects to arrays
+		 *
+		 * @param object $object
+		 *
+		 * @return array
+		 */
+	public static function ObjectArray($object) {
+		if(is_object($object)){
+  			$reflectionClass = new ReflectionClass(get_class($object));
+		    $array = [];
+		    foreach ($reflectionClass->getProperties() as $property) {
+		        $property->setAccessible(true);
+		        $array[$property->getName()] = $property->getValue($object);
+		        $property->setAccessible(false);
+		    }
+		    return $array;
+		}else{
+			return false;
+		}	  
+	}
+
+		/**
+		 * Convert XML to arrays
+		 *
+		 * @param xml object $xml xml
+		 *
+		 * @return array
+		 */
+	public static function XmlArray($xml){
+		$dom = simplexml_load_file($xml);
+		$json_encode = json_encode($dom);
+		$json_decode = json_decode($json_encode,true);
+		return $json_decode;
+
+	}	
+
+}
