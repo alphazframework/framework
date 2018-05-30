be9c7c67130103dfc988f90ea1985262ae12c46f
malikumer <lablnet01@gmail.com>
2018-05-30 21:01:47 +0500

    adding two more classes"

---
+++ Common/OperatingSystem.php
@@ -0,0 +1,40 @@
+<?php
+namespace Softhub99\Zest_Framework\Common;
+
+class OperatingSystem{
+
+	public static function get(){
+		if(\define(PHP_OS_FAMILY)){
+			return PHP_OS_FAMILY;
+		}else{
+			return static::phpOs();
+		}
+	}
+	public static function phpOs(){
+		$os = PHP_OS;
+		switch ($os) {
+		 	case 'WINNT':
+		 	case 'WINNT32':
+		 	case 'WINNT64':
+		 	case 'Windows':
+		 		$c_os = "Windows";	
+		 		break;
+		 	case 'DragonFly':
+            case 'FreeBSD':
+            case 'NetBSD':
+            case 'OpenBSD':
+                $c_os = 'BSD';
+                break;
+            case 'Linux':
+                $c_os = 'Linux';
+                break;
+            case 'SunOS':
+                $c_os = 'Solaris';
+                break;	 	
+		 	default:
+		 		$c_os = "Unknown";	
+		 		break;
+		 } 
+		 return $c_os;
+	}
+}
