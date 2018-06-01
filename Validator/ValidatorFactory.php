1db69a543e29a9b9d0e952bf9dfdae8b0add403c
Malik Umer Farooq <Lablnet01@gmail.com>
2018-06-01 05:34:10 +0000

    Apply fixes from StyleCI

--- Validator/ValidatorFactory.php
+++ Validator/ValidatorFactory.php
@@ -4,10 +4,10 @@ namespace Softhub99\Zest_Framework\Validator;
 
 use Softhub99\Zest_Framework\Validator\Validator as Validator;
 
-class ValidatorFactory {
-
-    public function make($rules) {
-		return new Validator($rules);
+class ValidatorFactory
+{
+    public function make($rules)
+    {
+        return new Validator($rules);
     }
-
 }
