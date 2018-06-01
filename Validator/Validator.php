2acc6c12a082fe29db5ef05d8c9049fdccd7778c
Malik Umer Farooq <Lablnet01@gmail.com>
2018-06-01 04:30:49 +0000

    Apply fixes from StyleCI

--- Validator/Validator.php
+++ Validator/Validator.php
@@ -1,8 +1,10 @@
-<?php 
+<?php
+
 namespace Softhub99\Zest_Framework\Validator;
+
 class Validator
 {
-	protected static $messages;
+    protected static $messages;
 
     protected static $handler;
 
@@ -12,47 +14,54 @@ class Validator
 
     public function __construct(array $data, array $rules, $customValidationErrorMessages = [])
     {
-    	static::create($data, $rules, $customValidationErrorMessages);
+        static::create($data, $rules, $customValidationErrorMessages);
     }
+
     /**
-    * Perform validation for data
-    * @param $data 
-    **/
-    public function parseData(array $data){
-    	$field = $data['field'];
-         foreach ($data['policies'] as $rule => $policy){
-            $passes = call_user_func_array([new Rule, $rule], [$field, $data['value'], $policy]);
-            if(!$passes){
+     * Perform validation for data.
+     *
+     * @param $data
+     **/
+    public function parseData(array $data)
+    {
+        $field = $data['field'];
+        foreach ($data['policies'] as $rule => $policy) {
+            $passes = call_user_func_array([new Rule(), $rule], [$field, $data['value'], $policy]);
+            if (!$passes) {
                 Handler::set(
                     str_replace(
                         [':attribute', ':policy', '_'],
                         [$field, $policy, ' '], static::$messages[$rule]), $field
                 );
             }
-        }   	
+        }
     }
-     /**
-	 * Create the error msg
-	 * @param
-	 * $data, fields and values pair under validation
+
+    /**
+     * Create the error msg.
+     *
+     * @param
+     * $data, fields and values pair under validation
      * $policies, the rules that validation must satisfy
      * $messages, custom validation messages
-	 * @return void	
+     *
+     * @return void
      **/
-     public function create(array $data, array $policies, array $messages = [])
+    public function create(array $data, array $policies, array $messages = [])
     {
         static::$customMessages = $messages;
         static::$messages = Handler::getValidationMessages(static::$customMessages);
-        foreach ($data as $field => $value){
-            if(in_array($field, array_keys($policies))){
-               static::parseData(
+        foreach ($data as $field => $value) {
+            if (in_array($field, array_keys($policies))) {
+                static::parseData(
                     ['field' => $field, 'value' => $value, 'policies' => $policies[$field]]
                 );
             }
         }
-    }   
-     /**
-     * Check if validation failed
+    }
+
+    /**
+     * Check if validation failed.
      *
      * @return bool
      */
@@ -62,7 +71,7 @@ class Validator
     }
 
     /**
-     * Set the error messages
+     * Set the error messages.
      *
      * @return $this
      */
@@ -72,44 +81,50 @@ class Validator
     }
 
     /**
-     * Check if a given key exists in array
+     * Check if a given key exists in array.
      *
      * @param $key
+     *
      * @return bool
      */
     public function isKey($key)
     {
-    	return (array_key_exists($key, static::$errors)) ? true : false;
+        return (array_key_exists($key, static::$errors)) ? true : false;
     }
 
     /**
-     * Get the first error in the validation error array for given key
+     * Get the first error in the validation error array for given key.
      *
      * @param  $key
+     *
      * @return mixed
      */
     public function first($key = null)
     {
         return current(static::$errors[$key]);
     }
+
     /**
-     * Get the last error in the validation error array for given key
+     * Get the last error in the validation error array for given key.
      *
      * @param  $key
+     *
      * @return mixed
      */
     public function last($key = null)
     {
         return end(static::$errors[$key]);
     }
+
     /**
-     * Get all error messages or all errors under a specified key
+     * Get all error messages or all errors under a specified key.
      *
      * @param $key
+     *
      * @return mixed
      */
     public function get($key = null)
     {
-    	return (isset($key)) ? static::$errors[$key] : static::$errors;
-    }   
+        return (isset($key)) ? static::$errors[$key] : static::$errors;
+    }
 }
