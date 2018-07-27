1990afa771130906eda243822a51be543e3e04fe
malikumer <lablnet01@gmail.com>
2018-07-27 13:15:33 +0500

    update root class

--- Common/Root.php
+++ Common/Root.php
@@ -8,15 +8,7 @@ class Root
 {
     public static function path()
     {
-        $path = realpath(dirname(__FILE__));
-        $patterns = [];
-        $patterns[0] = '/vendor\\\\/';
-        $patterns[1] = '/softhub99\\\\/';
-        $patterns[2] = '/zest_framework\\\\/';
-        $patterns[3] = '/Common/';
-        $root = preg_replace($patterns, '', $path);
-
-        return $root;
+         return '../';
     }
 
     public function rootPaths()
@@ -24,26 +16,26 @@ class Root
         $roots = [
             'root' => static::path(),
             //App
-            'app'         => static::path().'App\\',
-            'controllers' => static::path().'App\Controllers\\',
-            'local'       => static::path().'App\local\\',
-            'middleware'  => static::path().'App\Middleware\\',
-            'models'      => static::path().'App\Models\\',
-            'views'       => static::path().'App\Views\\',
+            'app'         => static::path().'App/',
+            'controllers' => static::path().'App/Controllers/',
+            'local'       => static::path().'App/local/',
+            'middleware'  => static::path().'App/Middleware/',
+            'models'      => static::path().'App/Models/',
+            'views'       => static::path().'App/Views/',
             //components
-            'com' => static::path().'App\Component\\',
+            'com' => static::path().'App/Component/',
             //config
-            'config' => static::path().'Config\\',
+            'config' => static::path().'Config/',
             //public
-            'public' => static::path().'public\\',
-            //routes
-            'routes' => static::path().'routes\\',
+            'public' => static::path().'public/',
+            //routes 
+            'routes' => static::path().'routes/',
             //Storage
-            'storage'         => static::path().'Storage\\',
-            'storage_backtup' => static::path().'Storage\Backup\\',
-            'storage_data'    => static::path().'Storage\Data\\',
-            'storage_logs'    => static::path().'Storage\logs\\',
-            'storage_session' => static::path().'Storage\Session\\',
+            'storage'         => static::path().'Storage/',
+            'storage_backtup' => static::path().'Storage/Backup/',
+            'storage_data'    => static::path().'Storage/Data/',
+            'storage_logs'    => static::path().'Storage/logs/',
+            'storage_session' => static::path().'Storage/Session/',
         ];
 
         return Conversion::arrayObject($roots);
