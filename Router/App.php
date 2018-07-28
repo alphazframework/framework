e2cacb399807733f06977a2df49624c82fa85bad
Malik Umer Farooq <Lablnet01@gmail.com>
2018-07-28 11:25:37 +0000

    Apply fixes from StyleCI

--- Router/App.php
+++ Router/App.php
@@ -10,43 +10,43 @@
  *  file that was distributed with this source code.
  *
  * @license MIT
- *
  */
 
-namespace Softhub99\Zest_Framework\Router; 
+namespace Softhub99\Zest_Framework\Router;
 
 class App extends \Softhub99\Zest_Framework\Router\Router
 {
-	
-	public function run()
-	{
-		$router = new Router;
-		if (\Config\Config::ROUTER_CACHE){
-			if ($this->isExpired()) {
-				$this->delete();
-				require_once '../routes/routes.php';
-			} else {
-				$router->routes = $router->loadCache();
-				$router->dispatch($_SERVER['QUERY_STRING']);
-			}
-		} else {
-			require_once '../routes/routes.php';
-		}
-	}
-	public function isExpired()
-	{
-		$f = fopen('../Storage/Cache/router_time.cache','r');
-		$expire = fread($f, filesize('../Storage/Cache/router_time.cache'));
-		fclose($f);
-		if ($expire <= time()) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	public function delete()
-	{
-		unlink("../Storage/Cache/routers.cache");
-		unlink("../Storage/Cache/router_time.cache");
-	}
+    public function run()
+    {
+        $router = new Router();
+        if (\Config\Config::ROUTER_CACHE) {
+            if ($this->isExpired()) {
+                $this->delete();
+                require_once '../routes/routes.php';
+            } else {
+                $router->routes = $router->loadCache();
+                $router->dispatch($_SERVER['QUERY_STRING']);
+            }
+        } else {
+            require_once '../routes/routes.php';
+        }
+    }
+
+    public function isExpired()
+    {
+        $f = fopen('../Storage/Cache/router_time.cache', 'r');
+        $expire = fread($f, filesize('../Storage/Cache/router_time.cache'));
+        fclose($f);
+        if ($expire <= time()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function delete()
+    {
+        unlink('../Storage/Cache/routers.cache');
+        unlink('../Storage/Cache/router_time.cache');
+    }
 }
