ffef2f461ba2256525e4eada0ef316bb275a214c
Malik Umer Farooq <Lablnet01@gmail.com>
2018-07-29 05:19:26 +0000

    Apply fixes from StyleCI

--- Validation/databaseRules.php
+++ Validation/databaseRules.php
@@ -1,20 +1,23 @@
-<?php 
+<?php
+
 namespace Softhub99\Zest_Framework\Validation;
-use Softhub99\Zest_Framework\Database\MYSQL as DB;
+
 use Config\Config;
+use Softhub99\Zest_Framework\Database\MYSQL as DB;
+
 class databaseRules extends StickyRules
 {
-	protected static $db_name = Config::DB_NAME;
+    protected static $db_name = Config::DB_NAME;
 
-	public function unique($column,$value,$table)
-	{
-		$db = new DB;
-		$result = $db->count(['db_name'=>static::$db_name,'table'=>$table,'wheres' => [$column.' ='."'{$value}'"]]);
-		$db->close();
-		if($result === 0) {	
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public function unique($column, $value, $table)
+    {
+        $db = new DB();
+        $result = $db->count(['db_name'=>static::$db_name, 'table'=>$table, 'wheres' => [$column.' ='."'{$value}'"]]);
+        $db->close();
+        if ($result === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
