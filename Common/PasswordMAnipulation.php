98541b69b80f54e4d073cff7482313feeeb40ff4
Malik Umer Farooq <Lablnet01@gmail.com>
2018-08-08 02:22:39 +0000

    Apply fixes from StyleCI

--- Common/PasswordMAnipulation.php
+++ Common/PasswordMAnipulation.php
@@ -19,21 +19,22 @@ class PasswordMAnipulation
     const PASS_MIN_LEN = 8;
 
     /**
-     * Generate the password
+     * Generate the password.
      *
      * @return mix-data
      */
     public function generatePassword()
     {
-        $salts =  (new \Softhub99\Zest_Framework\Site\Site())->salts(12);
+        $salts = (new \Softhub99\Zest_Framework\Site\Site())->salts(12);
         $special_char1 = '~<>?|:.(),';
         $special_char2 = '!@#$%^&*_+-*+';
         $pass = $special_char2.$salts.$special_char1;
 
         return str_shuffle($pass);
     }
+
     /**
-     * Match the password 
+     * Match the password.
      *
      * @param $password userPassword , $hash password hash
      *
@@ -43,8 +44,9 @@ class PasswordMAnipulation
     {
         return password_verify($password, $hash);
     }
+
     /**
-     * Hash the password
+     * Hash the password.
      *
      * @param $password userPassword
      *
@@ -54,8 +56,9 @@ class PasswordMAnipulation
     {
         return password_hash($password, PASSWORD_ARGON2I);
     }
+
     /**
-     * Validate the password
+     * Validate the password.
      *
      * @param $password userPassword
      *
@@ -65,8 +68,9 @@ class PasswordMAnipulation
     {
         return ($this->isU($password) && $this->isL($password) && $this->isN($password) && $this->isS($password) && $this->len($password) >= self::PASS_MIN_LEN) ? true : false;
     }
+
     /**
-     * Check is capital letter is included in password
+     * Check is capital letter is included in password.
      *
      * @param $password userPassword
      *
@@ -76,44 +80,45 @@ class PasswordMAnipulation
     {
         return preg_match('/[A-Z]/', $password);
     }
+
     /**
-     * Check is small letter is included in password
+     * Check is small letter is included in password.
      *
      * @param $password userPassword
      *
      * @return int
      */
-
     public function isL($password)
     {
         return preg_match('/[a-z]/', $password);
     }
+
     /**
-     * Check is the integer included in password
+     * Check is the integer included in password.
      *
      * @param $password userPassword
      *
      * @return int
      */
-
     public function isN($password)
     {
         return preg_match('/[0-9]/', $password);
     }
+
     /**
-     * Check is special character is in password
+     * Check is special character is in password.
      *
      * @param $password userPassword
      *
      * @return int
      */
-
     public function isS($password)
     {
         return preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $password);
     }
+
     /**
-     * Get password length 
+     * Get password length.
      *
      * @param $password userPassword
      *
