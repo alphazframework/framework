1a15fa70e7756031a0457b4721f39ca14368c2b1
Malik Umer Farooq <Lablnet01@gmail.com>
2018-08-19 02:46:49 +0000

    Apply fixes from StyleCI

--- src/Common/Container/Container.php
+++ src/Common/Container/Container.php
@@ -14,51 +14,54 @@
 
 namespace Zest\Common\Container;
 
-class Container 
-
+class Container
 {
     /**
-     * The singleton instance of this class
+     * The singleton instance of this class.
      */
     private static $instance;
     /**
      * The collection of dependencies contained.
      */
     private $dependencies;
+
     /**
-     * __construct
-     *
+     * __construct.
      */
     private function __construct()
     {
         $this->dependencies = [];
     }
+
     /**
      * Get the instance of self class.
      *
      * @return object
-     */    
+     */
     public static function getInstance()
     {
         if (self::$instance === null) {
             self::$instance = new self();
         }
+
         return self::$instance;
     }
+
     /**
      * Adds an object generator and the identifier for that object, with the option
      * of make the object 'singleton' or not.
      *
-     * @param string   $identifier The identifier of the dependency
-     *                 $loader     The generator for the dependency objectb (callable)
-     *                 $singleton  Whether or not to return always the same instance of the object 
-     * 
+     * @param string $identifier The identifier of the dependency
+     *                           $loader     The generator for the dependency objectb (callable)
+     *                           $singleton  Whether or not to return always the same instance of the object
+     *
      * @return void
      */
     public function add($identifier, callable $loader, $singleton = true)
     {
         $this->dependencies[$identifier] = new Dependency($loader, $singleton);
     }
+
     /**
      * Gets the dependency identified by the given identifier.
      *
@@ -69,15 +72,17 @@ class Container
     public function get($identifier)
     {
         if (!isset($this->dependencies[$identifier])) {
-            throw new \Exception("Dependency identified by '$identifier' does not exist",500);
+            throw new \Exception("Dependency identified by '$identifier' does not exist", 500);
         }
+
         return $this->dependencies[$identifier]->get();
     }
+
     /**
      * Returns all the dependencies.
      *
      * @return array
-     */        
+     */
     public function debug()
     {
         return $this->dependencies;
