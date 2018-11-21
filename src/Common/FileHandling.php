c79fd12e1e32a0097ac84b9206a4fe8cbbebbb75
Lablnet <lablnet01@gmail.com>
2018-11-20 18:11:30 -0800

    added fileHandling class

---
+++ src/Common/FileHandling.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * This file is part of the Zest Framework.
+ *
+ * @author   Malik Umer Farooq <lablnet01@gmail.com>
+ * @author-profile https://www.facebook.com/malikumerfarooq01/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ *
+ * @license MIT
+ */
+
+namespace Zest\Common;
+
+use Config\Config;
+
+class FileHandling
+{
+    /*
+     * resource
+    */
+    private $resource;
+    /*
+     * Mode of files
+    */
+    private $modes = [
+        'readOnly' => 'r',
+        'readWrite' => 'r+',
+        'writeOnly' => 'w',
+        'writeMaster' => 'w+',
+        'writeAppend' => 'a',
+        'readWriteAppend' => 'a+',
+    ];
+    /**
+     * Open the file.
+     *
+     * @param $name => name of file
+     *        $mode => mode of file
+     *
+     * @return resource
+     */
+    public function open($name,$mode)
+    {
+        if (!empty(escape($name))) {
+            $this->resource = fopen($name, $this->modes[$mode]);
+            return $this;
+        }
+    }
+    /**
+     * Read the file.
+     *
+     * @param $file file that to be read
+     *
+     * @return file
+     */    
+    public function read($file) 
+    {
+        return fread($this->resource, filesize($file));
+    }
+    /**
+     * Write on file.
+     *
+     * @param $string (string) string that you want write on file
+     *
+     * @return bool
+     */    
+    public function write(string $string)
+    {
+        return (!empty($string)) ? fwrite($this->resource, $string) : false;
+    }
+    /**
+     * Check if file is already or not.
+     *
+     * @param $file file
+     *
+     * @return bool
+     */    
+    public function isFile($file)
+    {
+        return (file_exists($file)) ? true : false;
+    }
+    /**
+     * Delete the file.
+     *
+     * @param $file file to be deleted
+     *
+     * @return bool
+     */    
+    public function delete($file)
+    {
+        return ($this->isFile($file)) ? unlink($file) : false;
+    }
+}
