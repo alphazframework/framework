78936ac3ca00137239d58664cd3efb0fb84bc5b4
Malik Umer Farooq <Lablnet01@gmail.com>
2018-11-28 07:36:11 +0000

    Apply fixes from StyleCI

--- src/functions/helpers.php
+++ src/functions/helpers.php
@@ -128,7 +128,7 @@ function read_file($file, $mode)
 {
     return (new \Zest\Files\FileHandling())->open($file, $mode)->read($file);
 }
-function pagination($total = 10,$perPage = 6,$current = 1 , $urlAppend = '/')
+function pagination($total = 10, $perPage = 6, $current = 1, $urlAppend = '/')
 {
-	return (new \Zest\Common\Pagination($total,$perPage,$current,$urlAppend))->pagination();
+    return (new \Zest\Common\Pagination($total, $perPage, $current, $urlAppend))->pagination();
 }
