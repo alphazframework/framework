610fb2219134539699e8e5d03c46d456d7f37302
Lablnet <lablnet01@gmail.com>
2019-02-11 17:24:24 +0500

    update

--- src/Expection/Expection.php
+++ src/Expection/Expection.php
@@ -15,62 +15,18 @@
  */
 
 namespace Zest\Expection;
-
-use Zest\Common\Logger\Logger;
-use Zest\View\View;
+use Zest\Expection\Whoops\Whoops;
 
 class Expection
 {
+    
     /**
-     * Error handler. Convert all errors to Exceptions by throwing an ErrorException.
+     * __construct
      *
-     * @param (int) $level Error level
-     *                     (string) $message Error message
-     *                     (string) $file    Filename the error was raised in
-     *                     (int)    $line    Line number in the file
-     *
-     * @since 1.0.0
-     *
-     * @return void
-     */
-    public static function errorHandler($level, $message, $file, $line)
+     * @since 3.0.0
+    */    
+    public function __construct()
     {
-        if (@error_reporting() !== 0) {
-            throw new \ErrorException($message, 0, $level, $file, $line);
-        }
-    }
-
-    /**
-     * Exception handler.
-     *
-     * @param (Exception) $exception The exception
-     *
-     * @since 1.0.0
-     *
-     * @return void
-     */
-    public static function exceptionHandler($exception)
-    {
-        // Code is 404 (not found) or 500 (general error)
-        $code = $exception->getCode();
-        if ($code != 404) {
-            $code = 500;
-        }
-        if (__config()->config->show_errors) {
-            echo '<h1>Fatal error</h1>';
-            echo "<p>Uncaught exception: '".get_class($exception)."'</p>";
-            echo "<p>Message: '".$exception->getMessage()."'</p>";
-            echo '<p>Stack trace:<pre>'.$exception->getTraceAsString().'</pre></p>';
-            echo "<p>Thrown in '".$exception->getFile()."' on line ".$exception->getLine().'</p>';
-        } else {
-            $logger = new Logger();
-            $log = date('Y-m-d').'.log';
-            $message = "Uncaught exception: '".get_class($exception)."'";
-            $message .= " with message '".$exception->getMessage()."'";
-            $message .= "\nStack trace: ".$exception->getTraceAsString();
-            $message .= "\nThrown in '".$exception->getFile()."' on line ".$exception->getLine();
-            $logger->setCustumFile($log)->error($message);
-            echo View::View("errors/$code");
-        }
+        (new Whoops());
     }
 }
