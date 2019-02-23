929e32deb8ab8fb6a693b43969d92bd8d764cf7b
Lablnet <lablnet01@gmail.com>
2019-02-23 07:27:20 +0500

    update

--- src/Router/App.php
+++ src/Router/App.php
@@ -3,8 +3,8 @@
 /**
  * This file is part of the Zest Framework.
  *
- * @author   Malik Umer Farooq <lablnet01@gmail.com>
- * @author-profile https://www.facebook.com/malikumerfarooq01/
+ * @author   Muhammad Umer Farooq <lablnet01@gmail.com>
+ * @author-profile https://www.facebook.com/Muhammadumerfarooq01/
  *
  * For the full copyright and license information, please view the LICENSE
  *  file that was distributed with this source code.
