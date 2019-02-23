a34d328ef53f99a21ea5156c181d734cef65d074
Muhammad Umer Farooq <Lablnet01@gmail.com>
2019-02-23 08:04:36 +0500

    Update FTP.php

--- src/Common/FTP.php
+++ src/Common/FTP.php
@@ -40,9 +40,9 @@ class FTP
      * Instantiate the FTP object.
      *
      * @param (string) $host server host
-     *                       (string) $user username
-     *                       (string) $pass password
-     *                       (string) $secured ftp or sftp
+     * @param (string) $user username
+     * @param (string) $pass password
+     * @param (string) $secured ftp or sftp
      *
      * @since 3.0.0
      */
@@ -118,7 +118,7 @@ class FTP
      * Change directories.
      *
      * @param (string) $dir directory
-     *                      (string) $new naw name
+     * @param (string) $new naw name
      *
      * @since 3.0.0
      *
@@ -219,8 +219,8 @@ class FTP
      * Get the file.
      *
      * @param (mixed) $local local
-     *                       (mixed)$remote remote
-     *                       (mixed) $mode mode
+     * @param (mixed)$remote remote
+     * @param (mixed) $mode mode
      *
      * @since 3.0.0
      *
@@ -235,7 +235,7 @@ class FTP
      * Rename file.
      *
      * @param (string) $old old
-     *                      (string) $new naw name
+     * @param (string) $new naw name
      *
      * @since 3.0.0
      *
@@ -250,7 +250,7 @@ class FTP
      * Change premission.
      *
      * @param (string) $file file
-     *                       (mixed) $mode mode
+     * @param (mixed) $mode mode
      *
      * @since 3.0.0
      *
@@ -307,7 +307,7 @@ class FTP
      * Upload the files.
      *
      * @param (array) $files number of files you want to uplaod
-     *                       (string) $root Server root directory or sub
+     * @param (string) $root Server root directory or sub
      *
      * @since 3.0.0
      *
