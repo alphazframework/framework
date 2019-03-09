e34429d398343044abb863677abc8e6bf7560e8b
Muhammad Umer Farooq <Lablnet01@gmail.com>
2019-03-09 06:20:55 +0500

    Update Components.php

--- src/Component/Components.php
+++ src/Component/Components.php
@@ -19,6 +19,8 @@ namespace Zest\Component;
 use Zest\Common\Version;
 use Zest\Data\Conversion;
 use Zest\Files\Files;
+use Zest\Files\FileHandling;
+use Zest\Zip\Zip;
 
 class Components
 {
@@ -50,30 +52,107 @@ class Components
         }
     }
 
-    public function install()
-    {
-    }
-
-    public function active($id)
-    {
-    }
-
-    public function disable($id)
-    {
-    }
-
     /**
-     * Determine whether the component is supported with current version of Zest.
+     * Install the component.
      *
-     * @param (string) $zVersion Zest version number from component config file.
+     * @param (string) $archive Valid component zip archive in Data folder.
+     * @param (string) $name    Name of component.
      *
      * @since 3.0.0
      *
      * @return bool
      */
-    public function isSupported($zVersion)
+    public function install($archive, $name)
     {
-        if (version_compare($zVersion, Version::VERSION, '<=') === true) {
+        $storageData = route()->storage_data;
+        $file = $storageData.$archive;
+        $files = new Files();
+        $files->mkdir($storageData.'tmp/');
+        $files->mkdir($storageData.'tmp/'.$name);
+        var_dump((new Zip())->extract($file,$storageData.'tmp/'.$name.'/', true));
+        if (file_exists($storageData.'tmp/'.$name.'/component.json')) {
+            $file = new FileHandling();
+            $c = $file->open($storageData.'/tmp/'.$name.'/component.json','readOnly')->read();
+            $file->close();
+            $config = json_decode($c, true);
+            if ($this->isSupported($config['zversion'], $config['comparator']) === true) {
+                if (!file_exists($route()->com.$name)) {
+                    $files->moveDir($storageData.'tmp/', route()->com, $name);
+                    return true;
+                }
+            } 
+        } 
+        return false;    
+    }
+
+    /**
+     * Activate or Disable component.
+     *
+     * @param (string) $name   Name of component.
+     * @param (bool)   $status Valid status.
+     *
+     * @since 3.0.0
+     *
+     * @return bool
+     */
+    public function activeOrDisable($name, $status)
+    {
+        $file = route()->com.$name.'/component.json';
+        if (file_exists($file)) {
+            $fileHandling = new FileHandling();
+            $c = $fileHandling->open($file, 'readOnly')->read();
+            $config = json_decode($c, true);
+            $config['status'] = $status;
+            $config = json_encode($config, JSON_PRETTY_PRINT);
+            $fileHandling->open($file, 'writeOnly')->write($config);
+            $fileHandling->close();
+            return true;
+        }
+
+        return false;        
+    }
+
+    /**
+     * Activate the component.
+     *
+     * @param (string) $name Name of component.
+     *
+     * @since 3.0.0
+     *
+     * @return bool
+     */
+    public function active($name)
+    {
+        $this->activeOrDisable($name, true);
+    }
+
+    /**
+     * Disable the component.
+     *
+     * @param (string) $name Name of component.
+     *
+     * @since 3.0.0
+     *
+     * @return bool
+     */
+    public function disable($name)
+    {
+        $this->activeOrDisable($name, false);
+    }
+
+    /**
+     * Determine whether the component is supported with current version of Zest.
+     *
+     * @param (string) $zVersion   Zest version number from component config file.
+     * @param (string) $comparator Operator that used to compare version.
+     *
+     * @since 3.0.0
+     *
+     * @return bool
+     */
+    public function isSupported($zVersion, $comparator)
+    {
+        if (version_compare($zVersion, Version::VERSION, $comparator) === true) {
             return true;
         }
 
