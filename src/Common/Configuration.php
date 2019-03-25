0a8abf7dd0a2f55221fd9d752c002942b279db19
peter279k <peter279k@gmail.com>
2019-03-25 20:29:38 +0800

    Fix basic hash tests

--- src/Common/Configuration.php
+++ src/Common/Configuration.php
@@ -44,15 +44,19 @@ class Configuration
     public function parseData()
     {
         $data = [];
-        $file1 = 'Config/App.php';
-        $file2 = '../Config/App.php';
+        $file1 = __DIR__.'/Config/App.php';
+        $file2 = __DIR__.'/../Config/App.php';
+        $file3 = __DIR__.'/../../config.php';
         if (file_exists($file1)) {
             $data += require $file1;
         } elseif (file_exists($file2)) {
             $data += require $file2;
+        } elseif (file_exists($file3)) {
+            $data += require $file3;
         } else {
             throw new \Exception("Error, while loading Config {$file1} file", 404);
         }
+
         return $data;
     }
 
