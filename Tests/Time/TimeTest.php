46044c19ebd0307174608334a89278fc7fc41518
Lablnet <lablnet01@gmail.com>
2019-03-30 19:46:23 +0500

    Added Time Package

---
+++ Tests/Time/TimeTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Framework\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Zest\Time\Time;
+
+class TimeTest extends TestCase
+{
+
+}
---
+++ src/Contracts/Time/Time.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the Zest Framework.
+ *
+ * @author   Muhammad Umer Farooq <lablnet01@gmail.com>
+ * @author-profile https://www.facebook.com/Muhammadumerfarooq01/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ *
+ * @since 3.0.0
+ *
+ * @license MIT
+ */
+
+namespace Zest\Contracts\Time;
+
+interface Time
+{
+
+	public function now();	
+}
