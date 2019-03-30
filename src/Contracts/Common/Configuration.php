6e47b2d9ad64144afcdddb41040cc44907109ea7
Lablnet <lablnet01@gmail.com>
2019-03-30 17:27:18 +0500

    Update Config Class

---
+++ src/Contracts/Common/Configuration.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * This file is part of the Zest Framework.
+ *
+ * @author   Muhammad Umer Farooq <lablnet01@gmail.com>
+ * @author-profile https://www.facebook.com/Muhammadumerfarooq01/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ *
+ * @since 3.0.0
+ *
+ * @license MIT
+ */
+
+namespace Zest\Contracts\Common;
+
+interface Configuration
+{
+    /**
+     * Determine if the given configuration value exists.
+     *
+     * @param  string  $key
+     *
+     * @since 3.0.0
+     *
+     * @return bool
+     */
+    public function has($key);
+
+    /**
+     * Get the specified configuration value.
+     *
+     * @param  string  $key
+     * @param  mixed   $default
+     *
+     * @since 3.0.0
+     *
+     * @return mixed
+     */
+    public function get($key, $default = null);
+
+    /**
+     * Set a given configuration value.
+     *
+     * @param  array|string  $key
+     * @param  mixed   $value
+     *
+     * @since 3.0.0
+     *
+     * @return void
+     */
+    public function set($key, $value = null);
+}
