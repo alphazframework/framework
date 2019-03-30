e2929858675d1f564fdd2fd4373db27b5c6f84d8
Lablnet <lablnet01@gmail.com>
2019-03-30 07:21:20 +0500

    fix trias built failure

--- Tests/Data/ArraysTest.php
+++ Tests/Data/ArraysTest.php
@@ -146,9 +146,9 @@ class ArraysTest extends TestCase
     public function testArrayChangeCaseKey()
     {
         $array = ['Name' => 'Alex'];
-        $this->assertTrue(['name' => 'A;ex'], Arrays::arrayChangeCaseKey($array));
-        $this->assertFalse(['Name' => 'A;ex'], Arrays::arrayChangeCaseKey($array));
-        $this->assertTrue(['name' => 'A;ex'], Arrays::arrayChangeCaseKey($array, CASE_UPPER));
-        $this->assertFalse(['NAME' => 'A;ex'], Arrays::arrayChangeCaseKey($array, CASE_UPPER));
+        $this->assertSame(['name' => 'A;ex'], Arrays::arrayChangeCaseKey($array));
+        $this->assertNotSame(['Name' => 'A;ex'], Arrays::arrayChangeCaseKey($array));
+        $this->assertSame(['name' => 'A;ex'], Arrays::arrayChangeCaseKey($array, CASE_UPPER));
+        $this->assertNotSame(['NAME' => 'A;ex'], Arrays::arrayChangeCaseKey($array, CASE_UPPER));
     }
 }
