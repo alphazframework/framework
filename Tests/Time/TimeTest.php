6976fccbdb84adf2bece5a90fadeb1a6f1d21df7
Muhammad Umer Farooq <Lablnet01@gmail.com>
2019-03-31 19:06:32 +0500

    Update TimeTest.php

--- Tests/Time/TimeTest.php
+++ Tests/Time/TimeTest.php
@@ -7,5 +7,9 @@ use Zest\Time\Time;
 
 class TimeTest extends TestCase
 {
-
+	public function testLeapYear()
+	{
+	 	$this->assertTrue(Time::isLeapYear(2020));
+	 	$this->assertFalse(Time::isLeapYear(2019));
+	}
 }
