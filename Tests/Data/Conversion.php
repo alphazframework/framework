eb62f4bf39e75ac272d57a10bae4b6826944af78
Muhammad Umer Farooq <Lablnet01@gmail.com>
2019-05-02 12:19:22 +0500

    Update Conversion.php

--- Tests/Data/Conversion.php
+++ Tests/Data/Conversion.php
@@ -7,11 +7,11 @@ use Zest\Data\Conversion;
 
 class ConversionTest extends TestCase
 {
-	public function testBitToBytes()
-	{
-		$this->assertSame('1K', Conversion::bitToBytes(1024));
-		$this->assertNotSame('1K', Conversion::bitToBytes(1024));
-		$this->assertSame('9.31G', Conversion::bitToBytes(1024));
-		$this->assertNotSame('9G', Conversion::bitToBytes(1024));
-	}
+    public function testBitToBytes()
+    {
+        $this->assertSame('1K', Conversion::bitToBytes(1024));
+        $this->assertNotSame('1K', Conversion::bitToBytes(1024));
+        $this->assertSame('9.31G', Conversion::bitToBytes(1024));
+        $this->assertNotSame('9G', Conversion::bitToBytes(1024));
+    }
 }
