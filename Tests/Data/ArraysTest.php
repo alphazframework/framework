f21a5d30965b5c6e511e3f5aba363e25a606984f
Muhammad Umer Farooq <Lablnet01@gmail.com>
2019-05-03 17:59:08 +0500

    fix mistake in Tests

--- Tests/Data/ArraysTest.php
+++ Tests/Data/ArraysTest.php
@@ -202,11 +202,11 @@ class ArraysTest extends TestCase
            1 => 2,
            2 => 3,
            3 => 4,
-           4 => 5,
-           5 => 6,
-           6 => 7,
-           7 => 8,
-           8 => 9
+           5 => 5,
+           6 => 6,
+           8 => 7,
+           9 => 8,
+           10 => 9
         ], Arrays::removeDuplicates($dataSet3));
     }
 }
