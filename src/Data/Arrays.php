d1c7d8b72190a1ef976a4cb3d9fcae1f6b89b8ea
Muhammad Umer Farooq <Lablnet01@gmail.com>
2019-05-13 18:32:56 +0500

    Added getType method

--- src/Data/Arrays.php
+++ src/Data/Arrays.php
@@ -78,6 +78,33 @@ class Arrays implements ArraysContract
         return isset($array[0]) && is_array($array[0]);
     }
 
+     /**
+     * Get type of array.
+     *
+     * @param array $array The array to work on.
+     *
+     * @since 3.0.0
+     *
+     * @return mixed
+     */   
+    public static function getType(array $array)
+    {
+        if (self::isReallyArray($array)) {
+            if (self::isSequential($array)) {
+                $type = 'indexes';
+            } elseif (self::isAssoc($array)) {
+                $type = 'assoc';
+            } elseif (self::isMulti($array)) {
+                $type = 'multi';
+            }
+
+            return isset($type) ? $type : false;
+        }
+
+        throw new \InvalidArgumentException("The given array should not be empty", 500);
+
+    }
+
     /**
      * Add an element to an array using "dot" notation if it doesn't exist.
      *
