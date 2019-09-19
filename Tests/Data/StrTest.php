04cf092250b705f7a5196aad3ed38b2dcbf9599b
Muhammad Umer Farooq <Lablnet01@gmail.com>
2019-09-18 21:17:12 -0700

    Create StrTest.php

---
+++ Tests/Data/StrTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Framework\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Zest\Data\Str;
+
+class StrTest extends TestCase
+{
+    public function testReverse()
+    {
+        $this->assertSame('olleh', Str::reverse('hello'));
+        $this->assertNotSame('hello', Str::reverse('hello'));
+    }
+
+    public function testConcat()
+    {
+        $this->assertSame("this is a book", Str::concat(' ' ,'this', ' is', ' a', ' book'));
+        $this->assertNotSame("thisisabook", Str::concat(' ' ,'this', ' is', ' a', ' book'));
+    }
+
+    public function testCount()
+    {
+        $this->assertSame(5, Str::count('hello'));
+        $this->assertNotSame(9, Str::count('hello'));
+    }
+}
