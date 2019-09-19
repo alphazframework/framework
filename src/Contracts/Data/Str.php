ae4645257b77b83cbd46d7ffa687379d24f7ba10
Muhammad Umer Farooq <Lablnet01@gmail.com>
2019-09-18 21:16:18 -0700

    Update Str.php

--- src/Contracts/Data/Str.php
+++ src/Contracts/Data/Str.php
@@ -39,7 +39,7 @@ interface Str
      *
      * @return bool
      */
-    public static function strConcat($g, ...$str);
+    public static function concat($g, ...$str);
 
     /**
      * Count the string.
