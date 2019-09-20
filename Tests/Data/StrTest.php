33355593a99e4914ed416ec607eab330fa173485
Muhammad Umer Farooq <Lablnet01@gmail.com>
2019-09-20 15:15:22 +0500

    Update StrTest.php

--- Tests/Data/StrTest.php
+++ Tests/Data/StrTest.php
@@ -15,7 +15,7 @@ class StrTest extends TestCase
 
     public function testConcat()
     {
-        $this->assertSame('this is a book', Str::concat(' ', 'this', ' is', ' a', ' book'));
+        $this->assertSame('this  is  a  book', Str::concat(' ', 'this', ' is', ' a', ' book'));
         $this->assertNotSame('thisisabook', Str::concat(' ', 'this', ' is', ' a', ' book'));
     }
 
