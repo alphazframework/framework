1d26ce8bdac07e0e964e5efe6500b83413e045d3
Hasmukh K Mistry <hasmukhmistry137@gmail.com>
2019-10-02 08:02:09 +0530

    Adds convert case method in str class and adds tests (#267)
    * Adds ConvertCase methods and test cases
    
    * Adds missing parameter information in function documentation
    
    * Formates the code to align the standard
    
    * Aligns the code standard
    
    * Returns value when mbstring is not active

--- src/Data/Str.php
+++ src/Data/Str.php
@@ -141,4 +141,32 @@ class Str implements StrContract
 
         return false;
     }
+
+    /**
+     * Change case of character to opposite case.
+     *
+     * @param string $str      String to be changed.
+     * @param string $encoding Valid encoding.
+     *
+     * @return string
+     */
+    public static function ConvertCase(string $str, $encoding = null)
+    {
+        if (function_exists('mb_strtolower') && function_exists('mb_strtoupper')) {
+            $characters = preg_split('/(?<!^)(?!$)/u', $str);
+            foreach ($characters as $key => $character) {
+                if (mb_strtolower($character, self::encoding($encoding)) !== $character) {
+                    $character = mb_strtolower($character, self::encoding($encoding));
+                } else {
+                    $character = mb_strtoupper($character, self::encoding($encoding));
+                }
+                $characters[$key] = $character;
+            }
+
+            return implode('', $characters);
+        }
+
+        // returns original string when mbstring is not active.
+        return $str;
+    }
 }
