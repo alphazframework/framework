a14e6802b9b29082556c3ba07b4ca51548d9b86d
Muhammad Umer Farooq <Lablnet01@gmail.com>
2019-10-03 12:42:35 +0500

    Update PasswordManipulationTest.php

--- Tests/Common/PasswordManipulationTest.php
+++ Tests/Common/PasswordManipulationTest.php
@@ -7,33 +7,33 @@ use Zest\Common\PasswordManipulation;
 
 class PasswordManipulationTest extends TestCase
 {
-    public function generatePasswordTest()
+    public function testGeneratePassword()
     {
         $pass = new PasswordManipulation();
         $this->assertNotEmpty($pass->generatePassword(11));
     }
 
-    public function countTes()
+    public function testCountTes()
     {
         $pass = new PasswordManipulation();
         $this->assertSame(6, $pass->len(123456));
     }
 
-    public function isValidTest()
+    public function testIsValid()
     {
         $pass = new PasswordManipulation();
         $this->assertTrue($pass->isValid($pass->generatePassword(15)));
         $this->assertFalse($pass->isValid('1234dasd'));
     }
 
-    public function setLengthTest()
+    public function testSetLength()
     {
         $pass = new PasswordManipulation();
         $pass->setLength(21);
         $this->assertNotEmpty($this->getLength());
     }
 
-    public function getLengthTest()
+    public function testGetLength()
     {
         $pass = new PasswordManipulation();
         $this->assertNotEmpty($this->getLength());
