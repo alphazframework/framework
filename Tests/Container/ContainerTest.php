91e1c8b537359dc9ac78c5a8584b55b385b016ca
Hasmukh Mistry <hasmukh.mistry@trivago.com>
2019-10-04 12:20:01 +0200

    Adds test for container and modifies class functions

--- Tests/Container/ContainerTest.php
+++ Tests/Container/ContainerTest.php
@@ -9,6 +9,16 @@ class ContainerTest extends TestCase
 {
     public function testContainer()
     {
+        $container = new Container();
+        $msg       = new \Zest\http\Message();
+
+        $instance = $container->register(['Zest\http\Message', 'Message'], $msg, false);
+
+        $this->assertFalse($container->isSingleton('Message'));
+
+        $instance2 = $container->register(['Zest\http\Message', 'Message'], $msg, true);
+
+        $this->assertTrue($container->isSingleton('Message'));
         // adds test cases here.
     }
 }
