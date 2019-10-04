2e5c23903e716bc8630f52bbe78713ea6791023a
Muhammad Umer Farooq <Lablnet01@gmail.com>
2019-10-04 14:37:01 +0500

    Apply fixes from StyleCI (#292)

--- Tests/Site/KeyTest.php
+++ Tests/Site/KeyTest.php
@@ -7,23 +7,24 @@ use Zest\Site\Key;
 
 class KeyTest extends TestCase
 {
-	public function testGenerate(): void
-	{
-		$key = Key::generate(16);
-		$this->assertEquals(16, mb_strlen($key, '8bit'));
+    public function testGenerate(): void
+    {
+        $key = Key::generate(16);
+        $this->assertEquals(16, mb_strlen($key, '8bit'));
     }
 
-	public function testEncodeAndDecode(): void
-	{
-		$key = Key::generate(16);
-		$this->assertEquals(16, mb_strlen($key, '8bit'));
-		$encoded = Key::encode($key);
-		$this->assertEquals(36, mb_strlen($encoded, '8bit'));
-		$this->assertEquals($key, Key::decode($encoded));
+    public function testEncodeAndDecode(): void
+    {
+        $key = Key::generate(16);
+        $this->assertEquals(16, mb_strlen($key, '8bit'));
+        $encoded = Key::encode($key);
+        $this->assertEquals(36, mb_strlen($encoded, '8bit'));
+        $this->assertEquals($key, Key::decode($encoded));
     }
-	public function testGenerateEncoded(): void
-	{
-		$key = Key::generateEncoded(16);
-        	$this->assertEquals(36, mb_strlen($key, '8bit'));
-	}
+
+    public function testGenerateEncoded(): void
+    {
+        $key = Key::generateEncoded(16);
+        $this->assertEquals(36, mb_strlen($key, '8bit'));
+    }
 }
--- src/Contracts/Site/Key.php
+++ src/Contracts/Site/Key.php
@@ -4,11 +4,11 @@
  * This file is part of the Zest Framework.
  *
  * @author   Muhammad Umer Farooq <lablnet01@gmail.com>
+ *
  * @link https://www.facebook.com/Muhammadumerfarooq01/
  *
  * For the full copyright and license information, please view the LICENSE
  *  file that was distributed with this source code.
- *
  * @since 3.0.0
  *
  * @license MIT
@@ -18,47 +18,47 @@ namespace Zest\Contracts\Site;
 
 interface Key
 {
-	/**
-	 * Converts a binary key into its hexadecimal representation.
-	 *
-	 * @param string $key Binary key
+    /**
+     * Converts a binary key into its hexadecimal representation.
+     *
+     * @param string $key Binary key
      *
      * @since 3.0.0
-     * 
-	 * @return string
-	 */
+     *
+     * @return string
+     */
     public static function encode(string $key) :string;
 
-	/**
-	 * Converts a hexadecimal key into its binary representation.
-	 *
-	 * @param string $key Binary key
-     * 
+    /**
+     * Converts a hexadecimal key into its binary representation.
+     *
+     * @param string $key Binary key
+     *
      * @since 3.0.0
      *
-	 * @return string
-	 */
+     * @return string
+     */
     public static function decode(string $key) :string;
 
-	/**
-	 * Generates a key.
-	 *
-	 * @param int $length Key length
+    /**
+     * Generates a key.
+     *
+     * @param int $length Key length
      *
      * @since 3.0.0
      *
-	 * @return string
-	 */
-	public static function generate(int $length = 32): string;
+     * @return string
+     */
+    public static function generate(int $length = 32): string;
 
-	/**
-	 * Generates a hex encoded key.
-	 *
-	 * @param int $length Key length
+    /**
+     * Generates a hex encoded key.
+     *
+     * @param int $length Key length
      *
      * @since 3.0.0
      *
-	 * @return string
-	 */
+     * @return string
+     */
     public static function generateEncode(int $length = 32) :string;
 }
