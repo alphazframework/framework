10a68c1be1b4cb5ed21ec22e429a6f09348c49ee
Hasmukh Mistry <hasmukh.mistry@trivago.com>
2019-10-08 15:09:31 +0200

    Adds more test for handling invalid class exception for container test

--- Tests/Container/ContainerTest.php
+++ Tests/Container/ContainerTest.php
@@ -4,21 +4,27 @@ namespace Framework\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Zest\Container\Container;
+use Zest\http\Message;
 
 class ContainerTest extends TestCase
 {
     public function testContainer()
     {
         $container = new Container();
-        $msg = new \Zest\http\Message();
+        $msg = new Message();
 
-        $instance = $container->register(['Zest\http\Message', 'Message'], $msg, false);
+        $instance = $container->register([Message::class, 'Message'], $msg, false);
 
         $this->assertFalse($container->isSingleton('Message'));
 
-        $instance2 = $container->register(['Zest\http\Message', 'Message'], $msg, true);
+        $instance2 = $container->register([Message::class, 'Message'], $msg, true);
 
         $this->assertTrue($container->isSingleton('Message'));
-        // adds test cases here.
+
+        try {
+            $instance3 = $container->register(['Invalid', 'Invalid'], null, true);
+        } catch (\InvalidArgumentException $e) {
+            $this->assertSame('Claass should be valid instance of Invalid.', $e->getMessage());
+        }
     }
 }
