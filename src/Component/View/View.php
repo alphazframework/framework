20ae0abb7e3c976d619f3c1bd972722236c579ac
Muhammad Umer Farooq <Lablnet01@gmail.com>
2019-10-17 14:54:30 +0500

    Apply fixes from StyleCI (#304)

--- src/Component/View/View.php
+++ src/Component/View/View.php
@@ -4,6 +4,7 @@
  * This file is part of the Zest Framework.
  *
  * @author Muhammad Umer Farooq (Malik) <mumerfarooqlablnet01@gmail.com>
+ *
  * @link https://lablnet.github.io/profile/
  *
  * For the full copyright and license information, please view the LICENSE
--- src/Cookies/Cookies.php
+++ src/Cookies/Cookies.php
@@ -4,11 +4,11 @@
  * This file is part of the Zest Framework.
  *
  * @author Muhammad Umer Farooq (Malik) <mumerfarooqlablnet01@gmail.com>
+ *
  * @link https://lablnet.github.io/profile/
  *
  * For the full copyright and license information, please view the LICENSE
  *  file that was distributed with this source code.
- *
  * @since 1.0.0
  *
  * @license MIT
