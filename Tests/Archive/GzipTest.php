8b5cb4faf165d1551340cf1acaae1d807a3f54bb
Caleb Mazalevskis <maikuolan@gmail.com>
2020-11-25 15:42:36 +0800

    An attempt to write some tests for the archive classes.

--- Tests/Archive/GzipTest.php
+++ Tests/Archive/GzipTest.php
@@ -9,11 +9,18 @@ class GzipTest extends TestCase
 {
     public function testExtract()
     {
-        //Todo
+        $Path = __DIR__ . DIRECTORY_SEPARATOR . 'Zest.png';
+        $Archive = new Gzip();
+        $Archive->compress($Path, __DIR__ . DIRECTORY_SEPARATOR . 'Zest.png.gz');
+        $Results = $Archive->extract(__DIR__ . DIRECTORY_SEPARATOR . 'Zest.png.gz', __DIR__ . DIRECTORY_SEPARATOR . 'Zest.png.new');
+        $this->assertTrue($Results);
     }
 
     public function testCompress()
     {
-       //Todo
+        $Path = __DIR__ . DIRECTORY_SEPARATOR . 'Zest.png';
+        $Archive = new Gzip();
+        $Results = $Archive->compress($Path, __DIR__ . DIRECTORY_SEPARATOR . 'Zest.png.gz');
+        $this->assertTrue($Results);
     }
 }
--- Tests/Archive/ZipTest.php
+++ Tests/Archive/ZipTest.php
@@ -9,11 +9,18 @@ class ZipTest extends TestCase
 {
     public function testExtract()
     {
-        //Todo
+        $Path = __DIR__ . DIRECTORY_SEPARATOR . 'Zest.png';
+        $Archive = new Zip();
+        $Archive->compress($Path, __DIR__ . DIRECTORY_SEPARATOR . 'Zest.png.zip');
+        $Results = $Archive->extract(__DIR__ . DIRECTORY_SEPARATOR . 'Zest.png.zip', __DIR__ . DIRECTORY_SEPARATOR . 'new');
+        $this->assertTrue($Results);
     }
 
     public function testCompress()
     {
-       //Todo
+        $Path = __DIR__ . DIRECTORY_SEPARATOR . 'Zest.png';
+        $Archive = new Zip();
+        $Results = $Archive->compress($Path, __DIR__ . DIRECTORY_SEPARATOR . 'Zest.png.zip');
+        $this->assertTrue($Results);
     }
 }
